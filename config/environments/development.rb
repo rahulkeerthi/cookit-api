40f265868e9fbf7f50d21b433979ac94ff8dbdef
Rahul Keerthi <rahulkeerthi2@gmail.com>
2020-06-29 23:39:28 +0100

    heroku config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,4 +50,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,7 +102,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.hosts << /\S*cookit\S*.vercel.app/
+  config.hosts << "api-cookit.herokuapp.com"
+  config.allowed_request_origins << /\S*cookit\S*.vercel.app/
   config.hosts << "cookit.vercel.app"
   config.hosts << "https://cookit-git-master.cookit.vercel.app"
   config.hosts << "vercel.app"
