0b76b898a731cda78c96917fff64b2db4c208765
Rahul Keerthi <rahulkeerthi2@gmail.com>
2020-06-30 00:21:01 +0100

    regex added for cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true, logger: (-> { Rails.logger }) do
   allow do
-    origins 'cookit.vercel.app'
+    origins /\D*cookit\D*.vercel.app/
 
     resource '*',
       headers: :any,
